0839ea20b7298698d2ea9d648c1f462a6226be80
mohammad hassan <sm.yaghoubi.mh@gmail.com>
2021-10-30 17:29:18 +0330

    its worked testing axios and alert data from api

--- Components/Pages/Search.js
+++ Components/Pages/Search.js
@@ -1,13 +1,25 @@
 import React from "react";
 import { View, Text, StyleSheet, StatusBar, Button, TouchableOpacity } from "react-native";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
+import axios from "axios";
+
+function GetDataFromMarketWatch(){
+    axios.get("http://192.168.1.4:80/MarketWatchApi")
+    .then((res)=>alert(JSON.stringify(res))).catch((err)=>alert(err))
+}
 
 function Search(){
     return ( 
         <View>
-            <Text>
-                hi
-            </Text>
+            <TouchableOpacity style={StyleSheet.create({
+                width :200,
+                height : 100,
+                backgroundColor: "red"
+            })}
+            onPress={GetDataFromMarketWatch}
+            >
+
+            </TouchableOpacity>
         </View>
     )
 }
