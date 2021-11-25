a43c64ed0c564de3feedd74e3e6a2153d576a7e1
mohammad hassan <sm.yaghoubi.mh@gmail.com>
2021-11-25 18:15:44 +0330

    create search component

--- Components/Pages/Search.js
+++ Components/Pages/Search.js
@@ -1,27 +1,58 @@
-import React from "react";
-import { View, Text, StyleSheet, StatusBar, Button, TouchableOpacity } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, StatusBar, TouchableOpacity, FlatList } from "react-native";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
+import { SearchBar } from "react-native-elements";
 import axios from "axios";
 
-function GetDataFromMarketWatch(){
-    axios.get("http://192.168.1.4:80/MarketWatchApi")
-    .then((res)=>alert(JSON.stringify(res))).catch((err)=>alert(err))
-}
+function SearchPage() {
+    const [data, setData] = useState([])
+    const [search, setSearch] = useState('')
+    const [abcd,setAbcd] = useState([])
+    async function GetDataFromMarketWatchApi() {
 
-function Search(){
-    return ( 
-        <View>
-            <TouchableOpacity style={StyleSheet.create({
-                width :200,
-                height : 100,
-                backgroundColor: "red"
-            })}
-            onPress={GetDataFromMarketWatch}
-            >
+        useEffect(() => {
+            const fetchData = async () => {
+                const result = await axios.get("http://192.168.1.4:80/MarketWatchApi")
+                setData(result.data)
+            }
+            fetchData()
+        }, [])
+    }
+    GetDataFromMarketWatchApi()
 
+
+    
+        const abc =data.filter(x=>x.namad == search)
+        
+  
+    return (
+        <View style={SearchPageStyles.SearchPageContainer}>
+            <SearchBar
+                onChangeText={(result) => setSearch(search + result)} />
+            <Text >{search}</Text>
+            <Text >{abcd.nam}</Text>
+            <TouchableOpacity
+                onPress={() => { setSearch("") }}>
+                <Text>reset</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                onPress={()=>{
+                    
+                    setAbcd(abc[0])
+                    console.log("lll",abcd)
+                    }}>
+                <Text>ok</Text>
             </TouchableOpacity>
         </View>
     )
 }
 
-export default Search
+const SearchPageStyles = StyleSheet.create({
+    SearchPageContainer: {
+        backgroundColor: "#5eff94",
+        width: wp("100%"),
+        height: hp("100%")
+    }
+})
+
+export default SearchPage
