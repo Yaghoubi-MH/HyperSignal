1fc7e02dd1dd0b2840124c7ebb0416bfce44e1e4
Yaghoubi <sm.yaghoubi.mh@gmail.com>
2022-01-10 17:26:03 +0330

    windows changed

--- Components/Home/Pages/Filter.js
+++ Components/Home/Pages/Filter.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Image, StatusBar, Button, TouchableOpacity } from "react-native";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen";
 import { useNavigation } from "@react-navigation/core";
 import PushNotification from "react-native-push-notification";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { color } from "react-native-elements/dist/helpers";
 
 const HeaderPage = () => {
     return (
@@ -13,10 +14,43 @@ const HeaderPage = () => {
     )
 }
 
+const MainContent = (props) => {
+    return (
+        <View>
+            <TouchableOpacity
+                style={props.LowRiskButtonColor}
+                onPress={props.LowRiskButtonOnPress}>
+                <Text style={MainContentStyles.LowRiskText}>کم ریسک</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={MainContentStyles.ConfirmButton}>
+                <Text style={MainContentStyles.LowRiskText}>اعمال فیلتر</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
 const Filter = () => {
+    const [LowRiskColor, setLowRiskColor] = useState(StyleSheet.create({ backgroundColor: "#aaa", }))
+    const [SelectedLowriskButton, setSelectedLowriskButton] = useState(false)
+    const [LowRiskLocalStorage, setLoRiskLocalStorage] = useState()
+
+    const OnPressHandlerLowRiskColor = () => {
+        if (!SelectedLowriskButton) {
+            setSelectedLowriskButton(true)
+            setLowRiskColor(StyleSheet.create({ backgroundColor: "#36D571" }))
+        } else {
+            setSelectedLowriskButton(false)
+            setLowRiskColor(StyleSheet.create({ backgroundColor: "#aaa" }))
+        }
+    }
+
     return (
         <View style={MainPageStyles.Container}>
             <HeaderPage />
+            <MainContent
+                LowRiskButtonColor={[LowRiskColor, MainContentStyles.LowRiskButton]}
+                LowRiskButtonOnPress={OnPressHandlerLowRiskColor} />
         </View>
     )
 }
@@ -28,12 +62,12 @@ const HeaderPageStyles = StyleSheet.create({
         height: hp(10),
         borderBottomRightRadius: 100,
         borderBottomLeftRadius: 100,
-        justifyContent : "center",
-        alignItems : "center"
+        justifyContent: "center",
+        alignItems: "center"
     },
     HeaderText: {
-        color : "#000",
-        fontSize : hp(6.29),
+        color: "#000",
+        fontSize: hp(6.29),
         fontWeight: "bold"
     }
 })
@@ -43,6 +77,32 @@ const MainPageStyles = StyleSheet.create({
         backgroundColor: "#2B283A",
         width: wp(100),
         height: hp(100),
+    }
+})
+
+const MainContentStyles = StyleSheet.create({
+    LowRiskButton: {
+        width: 300,
+        height: 50,
+        marginTop: 200,
+        marginLeft: 60,
+        borderRadius: 100,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    ConfirmButton: {
+        backgroundColor: "#feb000",
+        width: 300,
+        height: 50,
+        marginTop: 450,
+        marginLeft: 60,
+        borderRadius: 100,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    LowRiskText: {
+        color: "#000",
+        fontSize: hp(2.5)
     }
 })
 export default Filter
